c234e74c7c1c0e5267ff3e617fddb503956c66a5
Abdul Samad M J <samadmj4@gmail.com>
2023-09-27 23:15:08 +0530

    tailwind removed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <div>
       <h1>DJX Community Registration</h1>
+      <div className='relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12'>
+        hiasdasdasd
+        asdasd
+      </div>
     </div>
   )
 }
