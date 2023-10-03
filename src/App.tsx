ff639f8250bfca3b9ff3cd9f436bd4764d73f01e
Mubashir Angathil <mubashirangathil5142@gmail.com>
2023-10-03 16:31:19 +0530

    eslint, prettier and husky configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,159 @@
-import React from 'react'
-import { object, string, boolean, Output, optional, minLength, url, regex, startsWith } from 'valibot'
-import './App.css'
-import { API } from './utils/api'
+import React from "react";
+import {
+  object,
+  string,
+  boolean,
+  Output,
+  optional,
+  minLength,
+  url,
+  regex,
+  startsWith,
+} from "valibot";
+import "./App.css";
+import { API } from "./utils/api";
 
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const App: React.FC = () => {
+  const queryParameters = new URLSearchParams(window.location.search);
+  const discordId = queryParameters.get("discordId");
 
-  const queryParameters = new URLSearchParams(window.location.search)
-  const discordId = queryParameters.get("discordId")
-
-  const MySwal = withReactContent(Swal)
-  const [loading, setLoading] = React.useState(false)
+  const MySwal = withReactContent(Swal);
+  const [loading, setLoading] = React.useState(false);
 
   const formSchema = object({
-    fullName: string("Name must be a string", [minLength(1, 'Please enter your full name')]),
-    discordId: string("Discord username must be a string", [minLength(1, "Please enter your discord ID")]),
-    mobileNumber: string("Your mobile must be a number", [regex(/^\d{10}$/, "Please enter a valid mobile number")]),
-    github: optional(string("Github must be a string", [url("enter a valid url"), startsWith('https://github.com', 'Please enter a valid github url')])),
-    linkedin: optional(string("LinkedIn must be a string", [url("enter a valid url"), startsWith('https://www.linkedin.com', 'Please enter a valid linkedin url')])),
+    fullName: string("Name must be a string", [
+      minLength(1, "Please enter your full name"),
+    ]),
+    discordId: string("Discord username must be a string", [
+      minLength(1, "Please enter your discord ID"),
+    ]),
+    mobileNumber: string("Your mobile must be a number", [
+      regex(/^\d{10}$/, "Please enter a valid mobile number"),
+    ]),
+    github: optional(
+      string("Github must be a string", [
+        url("enter a valid url"),
+        startsWith("https://github.com", "Please enter a valid github url"),
+      ]),
+    ),
+    linkedin: optional(
+      string("LinkedIn must be a string", [
+        url("enter a valid url"),
+        startsWith(
+          "https://www.linkedin.com",
+          "Please enter a valid linkedin url",
+        ),
+      ]),
+    ),
     roles: object({
       frontEnd: boolean(),
-      backEnd: boolean()
-    })
-  })
+      backEnd: boolean(),
+    }),
+  });
 
-  type FormState = Output<typeof formSchema>
+  type FormState = Output<typeof formSchema>;
 
   const [formState, setFormState] = React.useState<FormState>({
-    fullName: '',
-    discordId: discordId ?? '',
-    mobileNumber: '',
-    github: '',
-    linkedin: '',
+    fullName: "",
+    discordId: discordId ?? "",
+    mobileNumber: "",
+    github: "",
+    linkedin: "",
     roles: {
       backEnd: false,
-      frontEnd: false
-    }
-  })
+      frontEnd: false,
+    },
+  });
 
   type FormErrors = {
-    [key in (keyof FormState)]?: string
-  }
+    [key in keyof FormState]?: string;
+  };
 
-  const [formErrors, setFormErrors] = React.useState<FormErrors>({})
+  const [formErrors, setFormErrors] = React.useState<FormErrors>({});
 
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setLoading(true)
-    let form: FormState = { ...formState }
-    Object.keys(formState).forEach(key => {
-      if (!['fullName', 'discordId', 'mobileNumber'].includes(key) && formState[key as keyof FormState] === '') {
-        form = { ...form, [key]: undefined }
+    e.preventDefault();
+    setLoading(true);
+    let form: FormState = { ...formState };
+    Object.keys(formState).forEach((key) => {
+      if (
+        !["fullName", "discordId", "mobileNumber"].includes(key) &&
+        formState[key as keyof FormState] === ""
+      ) {
+        form = { ...form, [key]: undefined };
       }
-    })
-    const result = formSchema._parse(form)
+    });
+    const result = formSchema._parse(form);
     if (result.issues && result.issues.length > 0) {
-      let issues: FormErrors = {}
+      let issues: FormErrors = {};
       for (const issue of result.issues) {
         if (issue.path) {
-          issues = { ...issues, [issue.path?.[0].key]: issue.message }
+          issues = { ...issues, [issue.path?.[0].key]: issue.message };
           // setFormErrors((prev: FormErrors) => ({ ...prev, [issue.path?.[0].key]: issue.message }))
         }
       }
-      setFormErrors(issues)
-      setLoading(false)
+      setFormErrors(issues);
+      setLoading(false);
     } else {
-      setFormErrors({})
+      setFormErrors({});
       API.register({
         fullname: formState.fullName,
         discordId: formState.discordId,
         phone: Number(formState.mobileNumber),
         github: formState.github,
         linkedin: formState.linkedin,
-        tech: Object.keys(formState.roles).filter(key => formState.roles[key as keyof FormState['roles']])
-      }).then(async (res) => {
-        setLoading(false)
-        if (res?.status === 200) {
-          MySwal.fire({
-            title: <p>{res?.message ?? 'Successfully registered!!'}</p>,
-            icon: 'success'
-          }).then(() => {
-            window.location.assign('https://discord.com/invite/g9ZEnxsWDz')
-          })
-        } else {
-          const response = await res.json()
-          MySwal.fire({
-            title: <p>{response?.message ?? 'Something went wrong. Please try again later!'}</p>,
-            icon: 'error'
-          })
-        }
-      }).catch((err) => {
-        console.log(err)
-        MySwal.fire({
-          title: <p>{err || 'Something went wrong. Please try again later!'}</p>,
-          icon: 'error' 
-        })
-        setLoading(false)
+        tech: Object.keys(formState.roles).filter(
+          (key) => formState.roles[key as keyof FormState["roles"]],
+        ),
       })
+        .then(async (res) => {
+          setLoading(false);
+          if (res?.status === 200) {
+            MySwal.fire({
+              title: <p>{res?.message ?? "Successfully registered!!"}</p>,
+              icon: "success",
+            }).then(() => {
+              window.location.assign("https://discord.com/invite/g9ZEnxsWDz");
+            });
+          } else {
+            const response = await res.json();
+            MySwal.fire({
+              title: (
+                <p>
+                  {response?.message ??
+                    "Something went wrong. Please try again later!"}
+                </p>
+              ),
+              icon: "error",
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          MySwal.fire({
+            title: (
+              <p>{err || "Something went wrong. Please try again later!"}</p>
+            ),
+            icon: "error",
+          });
+          setLoading(false);
+        });
     }
-  }
+  };
 
   React.useEffect(() => {
-    Object.keys(formState).forEach(key => {
-      const element = document.getElementById(key)
+    Object.keys(formState).forEach((key) => {
+      const element = document.getElementById(key);
       if (element && formErrors[key as keyof FormState]) {
-        element.classList.add('input-error')
+        element.classList.add("input-error");
       } else if (element) {
-        element.classList.remove('input-error')
+        element.classList.remove("input-error");
       }
-    })
-  }, [formErrors, formState])
+    });
+  }, [formErrors, formState]);
 
   return (
     <>
@@ -127,30 +169,63 @@ const App: React.FC = () => {
         <li></li>
         <li></li>
       </ul>
-      <div style={{ width: 'auto' }}>
-        <h1 style={{ position: 'relative' }}>DJX Community Registration 🚀</h1>
-        <div className='card'>
-          <form id='discordForm' onSubmit={submitHandler} autoComplete='off'>
-            <div className='form-item'>
-              <label htmlFor="fullName">Full Name<span className='required'>*</span></label>
-              <input className='text-field' type="text" name="fullName" id="fullName"
-                value={formState.fullName} onChange={e => setFormState({ ...formState, fullName: e.target.value })}
+      <div style={{ width: "auto" }}>
+        <h1 style={{ position: "relative" }}>DJX Community Registration 🚀</h1>
+        <div className="card">
+          <form id="discordForm" onSubmit={submitHandler} autoComplete="off">
+            <div className="form-item">
+              <label htmlFor="fullName">
+                Full Name<span className="required">*</span>
+              </label>
+              <input
+                className="text-field"
+                type="text"
+                name="fullName"
+                id="fullName"
+                value={formState.fullName}
+                onChange={(e) =>
+                  setFormState({ ...formState, fullName: e.target.value })
+                }
               />
-              {formErrors.fullName && <span className='error'>{formErrors.fullName}</span>}
+              {formErrors.fullName && (
+                <span className="error">{formErrors.fullName}</span>
+              )}
             </div>
-            <div className='form-item'>
-              <label htmlFor="discordId">Discord ID<span className='required'>*</span></label>
-              <input className='text-field' type="text" name="discordId" id="discordId"
-                value={formState.discordId} onChange={e => setFormState({ ...formState, discordId: e.target.value })}
+            <div className="form-item">
+              <label htmlFor="discordId">
+                Discord ID<span className="required">*</span>
+              </label>
+              <input
+                className="text-field"
+                type="text"
+                name="discordId"
+                id="discordId"
+                value={formState.discordId}
+                onChange={(e) =>
+                  setFormState({ ...formState, discordId: e.target.value })
+                }
               />
-              {formErrors.discordId && <span className='error'>{formErrors.discordId}</span>}
+              {formErrors.discordId && (
+                <span className="error">{formErrors.discordId}</span>
+              )}
             </div>
-            <div className='form-item'>
-              <label htmlFor="mobileNumber">Mobile Number<span className='required'>*</span></label>
-              <input className='text-field' type="text" name="mobileNumber" id="mobileNumber"
-                value={formState.mobileNumber} onChange={e => setFormState({ ...formState, mobileNumber: e.target.value })}
+            <div className="form-item">
+              <label htmlFor="mobileNumber">
+                Mobile Number<span className="required">*</span>
+              </label>
+              <input
+                className="text-field"
+                type="text"
+                name="mobileNumber"
+                id="mobileNumber"
+                value={formState.mobileNumber}
+                onChange={(e) =>
+                  setFormState({ ...formState, mobileNumber: e.target.value })
+                }
               />
-              {formErrors.mobileNumber && <span className='error'>{formErrors.mobileNumber}</span>}
+              {formErrors.mobileNumber && (
+                <span className="error">{formErrors.mobileNumber}</span>
+              )}
             </div>
             {/* <div className='form-item'>
               <label htmlFor="email">Email</label>
@@ -159,46 +234,95 @@ const App: React.FC = () => {
               />
               {formErrors.email && <span className='error'>{formErrors.email}</span>}
             </div> */}
-            <div className='form-item'>
+            <div className="form-item">
               <label htmlFor="github">Github</label>
-              <input className='text-field' type="url" name="github" id="github"
-                value={formState.github} onChange={e => setFormState({ ...formState, github: e.target.value })}
+              <input
+                className="text-field"
+                type="url"
+                name="github"
+                id="github"
+                value={formState.github}
+                onChange={(e) =>
+                  setFormState({ ...formState, github: e.target.value })
+                }
               />
-              {formErrors.github && <span className='error'>{formErrors.github}</span>}
+              {formErrors.github && (
+                <span className="error">{formErrors.github}</span>
+              )}
             </div>
-            <div className='form-item'>
+            <div className="form-item">
               <label htmlFor="linkedin">LinkedIn</label>
-              <input className='text-field' type="url" name="linkedin" id="linkedin"
-                value={formState.linkedin} onChange={e => setFormState({ ...formState, linkedin: e.target.value })}
+              <input
+                className="text-field"
+                type="url"
+                name="linkedin"
+                id="linkedin"
+                value={formState.linkedin}
+                onChange={(e) =>
+                  setFormState({ ...formState, linkedin: e.target.value })
+                }
               />
-              {formErrors.linkedin && <span className='error'>{formErrors.linkedin}</span>}
+              {formErrors.linkedin && (
+                <span className="error">{formErrors.linkedin}</span>
+              )}
             </div>
-            <div className='form-item'>
+            <div className="form-item">
               <label htmlFor="roles">Discord Server Roles</label>
-              <div id='roles'>
+              <div id="roles">
                 <span>
-                  <input type="checkbox" name="frontEnd" id="frontEnd"
-                    checked={formState.roles?.frontEnd} onChange={e => setFormState({ ...formState, roles: { ...formState.roles, frontEnd: e.target.checked } })}
-                  /> <label htmlFor="frontEnd">Front End</label>
+                  <input
+                    type="checkbox"
+                    name="frontEnd"
+                    id="frontEnd"
+                    checked={formState.roles?.frontEnd}
+                    onChange={(e) =>
+                      setFormState({
+                        ...formState,
+                        roles: {
+                          ...formState.roles,
+                          frontEnd: e.target.checked,
+                        },
+                      })
+                    }
+                  />{" "}
+                  <label htmlFor="frontEnd">Front End</label>
                 </span>
                 <span>
-                  <input type="checkbox" name="backEnd" id="backEnd"
-                    checked={formState.roles?.backEnd} onChange={e => setFormState({ ...formState, roles: { ...formState.roles, backEnd: e.target.checked } })}
-                  /> <label htmlFor="backEnd">Back End</label>
+                  <input
+                    type="checkbox"
+                    name="backEnd"
+                    id="backEnd"
+                    checked={formState.roles?.backEnd}
+                    onChange={(e) =>
+                      setFormState({
+                        ...formState,
+                        roles: {
+                          ...formState.roles,
+                          backEnd: e.target.checked,
+                        },
+                      })
+                    }
+                  />{" "}
+                  <label htmlFor="backEnd">Back End</label>
                 </span>
               </div>
             </div>
             {/* <div>
             <input type="checkbox" name="tandc" id="tandc" /> I agree to the <a href="#">Terms and Conditions</a>
           </div> */}
-            <div className='form-item'>
-              <input id='submitButton' type="submit" value="Submit" disabled={loading}/>
+            <div className="form-item">
+              <input
+                id="submitButton"
+                type="submit"
+                value="Submit"
+                disabled={loading}
+              />
             </div>
           </form>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,32 +1,31 @@
 export interface RegisterUser {
-    fullname: string
-    phone: number
-    discordId: string
-    github?: string
-    linkedin?: string
-    tech?: string[]
+  fullname: string;
+  phone: number;
+  discordId: string;
+  github?: string;
+  linkedin?: string;
+  tech?: string[];
 }
 
-
 export const API = {
-    register: async (user: RegisterUser) => {
-        const BASE_URL = import.meta.env.VITE_APP_API
-        const res = await fetch(BASE_URL + '/users/register', {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        })
-            .then((res) => {
-                return res
-            })
-            .catch((err) => {
-                console.log(err)
-                return err
-            })
-        return res
-    }
+  register: async (user: RegisterUser) => {
+    const BASE_URL = import.meta.env.VITE_APP_API;
+    const res = await fetch(BASE_URL + "/users/register", {
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+    return res;
+  },
 };
